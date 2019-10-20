9151c8908c9c50687c6a43a7bcd6fc663e851dff
sirius gurban <failoglu96@gmail.com>
2019-10-21 00:13:48 +0400

    notworking now_working

--- src/hw2.java
+++ src/hw2.java
@@ -2,14 +2,22 @@ package src;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class hw2 {
     public static void main (String[] args) {
 
 
+        Random rand = new Random();
+        int max = 5, min = 1;
+        int x = rand.nextInt((max - min) + 1) + min;
+        int y = rand.nextInt((max - min) + 1) + min;
 
-        String[][] numbers = {
+        System.out.println("Correct row: " + x);
+        System.out.println("Correct column: " + y);
+
+        String[][] numbers = new String[][]{
                 {"0","1","2","3","4","5"},
                 {"1","-","-","-","-","-",},
                 {"2","-","-","-","-","-",},
@@ -18,22 +26,60 @@ public class hw2 {
                 {"5","-","-","-","-","-",}
         };
 
-        //int strArray[][] = Arrays.toString(numbers);
-
-        Scanner input1 = new Scanner(System.in);
         int row;
-        System.out.println("Enter row: " + row);
-        row = input1.nextInt();
-        Scanner input2 = new Scanner(System.in);
         int column;
-        System.out.println("Enter column: " + column);
-        column = input2.nextInt();
-        int kkk;
+
+
+        for (int i=0; i<numbers.length;i++){
+            for (int j=0; j<numbers[i].length ;j++ ){
+                System.out.print(numbers[i][j] + " | ");
+            }
+            System.out.println();
+        }
+
+       do {
+           System.out.println("Enter row: ");
+           Scanner input1 = new Scanner(System.in);
+           row = input1.nextInt();
+           System.out.println("Enter column: ");
+           Scanner input2 = new Scanner(System.in);
+           column = input2.nextInt();
+
+
+
+           numbers[row][column] = "*";
+
+           for (int i=0; i<numbers.length;i++){
+               for (int j=0; j<numbers[i].length ;j++ ){
+                   System.out.print(numbers[i][j] + " | ");
+               }
+               System.out.println();
+           }
+
+
+       } while (numbers[x][y] != numbers[row][column]);{
+
+            numbers[row][column] = "x";
+
+            for (int i=0; i<numbers.length;i++){
+                for (int j=0; j<numbers[i].length ;j++ ){
+                    System.out.print(numbers[i][j] + " | ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("You have won!");
+        }
+
+
+
+
+
 
 
         //int[row][column]numbers = int[][];
 
-        for (int i=0; i<numbers.length;i++){
+      /*  for (int i=0; i<numbers.length;i++){
 
             for (int j=0; j<numbers[i].length ;j++ ){
 
@@ -42,7 +88,10 @@ public class hw2 {
             System.out.println();
         }
 
-        if (row>1 && column>1  && row<numbers.length && column<numbers.length)
+
+       */
+
+      /*  if (row>1 && column>1  && row<numbers.length && column<numbers.length)
         {
             int[row][column]numbers = int[][];
         }
@@ -50,7 +99,7 @@ public class hw2 {
         {
             System.out.println("Invalid input.......");
         }
-
+*/
 
     }
 
